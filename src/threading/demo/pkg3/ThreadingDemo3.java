40a55c3cc1a5a1d2c29e8e1d672d3586a0f44475
john graham <john.graham@outlook.co.nz>
2018-01-09 02:59:36 +1300

    Some improved formatting and commenting.

--- src/threading/demo/pkg3/ThreadingDemo3.java
+++ src/threading/demo/pkg3/ThreadingDemo3.java
@@ -1,30 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package threading.demo.pkg3;
 
 /**
  *
- * @author datas
+ * @author John Graha,
+ * @Date 9 January 2018
  */
 public class ThreadingDemo3 {
 
     /**
-     * @param args the command line arguments
+ * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //First thread anomymously implementing run();
         Thread thread1 = new Thread(() -> {
             /*
             *   Some arbitrary "work" (The sme as in an earlier repo.
-            */
+             */
             for (int i = 0; i < 10; i++) {
                 System.out.println("doing some work");
                 // Print the thread id to the console and notice it is different between threads.
                 System.out.println("Thread id = " + Thread.currentThread().getId());
             }
-            
+
             try {
                 // A small sleep
                 Thread.sleep(100);
@@ -35,16 +32,17 @@ public class ThreadingDemo3 {
             }
         });
 
+        //Second thread anomymously implementing run();
         Thread thread2 = new Thread(() -> {
             /*
             *   Some arbitrary "work" (The sme as in an earlier repo.
-            */
+             */
             for (int i = 0; i < 10; i++) {
                 System.out.println("doing some work");
                 // Print the thread id to the console and notice it is different between threads.
                 System.out.println("Thread id = " + Thread.currentThread().getId());
             }
-            
+
             try {
                 // A small sleep
                 Thread.sleep(100);
@@ -54,7 +52,7 @@ public class ThreadingDemo3 {
                 System.out.println(ex.getMessage());
             }
         });
-        
+        // start threads.
         thread1.start();
         thread2.start();
     }
